6beff642e782cfea15df71b100f909b95301c679
Juho Hautala <juho.hautala@helsinki.fi>
2017-03-20 22:27:41 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,46 @@
+;;
+;; Packages
+;;
+
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(let ((my/packages '(cider
+		      clojure-mode
+		      clojure-mode-extra-font-locking
+		      magit
+		      paredit
+		      smex
+		      tagedit)))
+  (dolist (p my/packages)
+    (unless (package-installed-p p)
+      (package-install p))))
+
+;; exec-path-from-shell
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+
+;;
+;; Keyboard
+;;
+
+;; set option key modifiers
+(setq mac-option-modifier 'none)
+(setq mac-right-option-modifier 'meta)
+
+
+;;
+;; Key bindings
+;;
+
+;; switch buffers
+(global-set-key (kbd "C-x C-n") 'next-buffer)
+(global-set-key (kbd "C-x C-p") 'previous-buffer)
